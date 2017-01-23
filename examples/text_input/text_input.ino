3e89eaa449ead07bd3ee8f1e0797ba259440c67e
Dakota Pekerti <dpekerti@optoro.com>
2017-01-23 15:48:42 -0500

    First Commit, added text_file

---
+++ examples/text_input/text_input.ino
@@ -0,0 +1,71 @@
+// testshapes demo for RGBmatrixPanel library.
+// Demonstrates the drawing abilities of the RGBmatrixPanel library.
+// For 32x64 RGB LED matrix.
+
+// NOTE THIS CAN ONLY BE USED ON A MEGA! NOT ENOUGH RAM ON UNO!
+
+#include <Adafruit_GFX.h>   // Core graphics library
+#include <RGBmatrixPanel.h> // Hardware-specific library
+
+#define OE   9
+#define LAT 10
+#define CLK 11
+#define A   A0
+#define B   A1
+#define C   A2
+#define D   A3
+
+RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false, 64);
+
+void setup() {
+
+  matrix.begin();
+
+  // draw some text!
+  matrix.setTextSize(1);     // size 1 == 8 pixels high
+  matrix.setTextWrap(false); // Don't wrap at end of line - will do ourselves
+
+  matrix.setCursor(8, 0);    // start at top left, with 8 pixel of spacing
+  uint8_t w = 0;
+  char *str = "AdafruitIndustries";
+  for (w=0; w<8; w++) {
+    matrix.setTextColor(Wheel(w));
+    matrix.print(str[w]);
+  }
+  matrix.setCursor(2, 8);    // next line
+  for (w=8; w<18; w++) {
+    matrix.setTextColor(Wheel(w));
+    matrix.print(str[w]);
+  }
+  matrix.println();
+  //matrix.setTextColor(matrix.Color333(4,4,4));
+  //matrix.println("Industries");
+  matrix.setTextColor(matrix.Color333(7,7,7));
+  matrix.println("LED MATRIX!");
+
+  // print each letter with a rainbow color
+  matrix.setTextColor(matrix.Color333(7,0,0));
+  matrix.println("Welcome!")
+
+
+  // whew!
+}
+
+void loop() {
+  // do nothing
+}
+
+
+// Input a value 0 to 24 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint16_t Wheel(byte WheelPos) {
+  if(WheelPos < 8) {
+   return matrix.Color333(7 - WheelPos, WheelPos, 0);
+  } else if(WheelPos < 16) {
+   WheelPos -= 8;
+   return matrix.Color333(0, 7-WheelPos, WheelPos);
+  } else {
+   WheelPos -= 16;
+   return matrix.Color333(0, WheelPos, 7 - WheelPos);
+  }
+}
